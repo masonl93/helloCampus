f25432b8c43f52c412701a37f4975a74481d2be9
Mason Levy <Mason@Mason-Levys-MacBook-Pro.local>
2016-03-06 16:56:23 -0800

    search bar on main activity

--- helloCampus/app/src/main/java/msj/hellocampus_android/MainActivity.java
+++ helloCampus/app/src/main/java/msj/hellocampus_android/MainActivity.java
@@ -5,13 +5,17 @@ import android.location.LocationListener;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.content.Intent;
 import android.net.Uri;
 import android.content.SharedPreferences.Editor;
 import android.util.Log;
+import android.widget.TextView;
 
 
 import com.firebase.client.AuthData;
@@ -29,9 +33,53 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         Firebase.setAndroidContext(this);
+        Firebase mPostsRef = new Firebase(getResources().getString(R.string.firebase_url));
 
         setContentView(R.layout.activity_main);
 
+        EditText editText = (EditText) findViewById(R.id.search);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    //search handled here
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+
+        //Button searchButton = (Button) findViewById(R.id./*insert*/);
+        /*
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
+
+        //ANYWALL POST BUTTON
+        /*
+        // Set up the handler for the search button click
+        Button postButton = (Button) findViewById(R.id.post_button);
+        postButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Only allow posts if we have a location
+                Location myLoc = (currentLocation == null) ? lastLocation : currentLocation;
+                if (myLoc == null) {
+                    Toast.makeText(MainActivity.this,
+                            "Please try again after your location appears on the map.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                intent.putExtra(Application.INTENT_EXTRA_LOCATION, myLoc);
+                startActivity(intent);
+            }
+        });
+        */
         /*
         imageView = (ImageView) this.findViewById(R.id.profile_image);
         Button selectImg = (Button) this.findViewById(R.id.select_image_button);
---
+++ helloCampus/app/src/main/java/msj/hellocampus_android/Post.java
@@ -0,0 +1,7 @@
+package msj.hellocampus_android;
+
+/**
+ * Created by Mason on 3/6/16.
+ */
+public class Post {
+}
